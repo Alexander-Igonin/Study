d619c9058ffb9c5d14d187895d3386434a330bca
Alexander-Igonin <55852571+Alexander-Igonin@users.noreply.github.com>
2019-10-01 20:19:20 +0300

    Add files via upload

---
+++ Lesson1/Lesson1.py
@@ -0,0 +1,20 @@
+print('\t\t*\t\t*\t\t\t* * * *\t\t\t\t*\t\t*'
+      '\n       * *\t\t*\t\t\t*\t\t\t\t\t  *   *'
+      '\n      *\t  *\t\t*\t\t\t*\t\t\t\t\t\t*'
+      '\n     * * * *\t*\t\t\t* * * *\t\t\t\t  *\t  *'
+      '\n\t*\t\t*\t*\t\t\t*\t\t\t\t\t*\t\t*'
+      '\n   *\t\t *  * * * *\t\t* * * *\t\t\t  *\t\t\t  *')
+
+print('Escape sequences \n',  '\\a      Bell (alert) \n', '\\b      Backspace\n', '\\n      New line\n',
+      '\\t      Horizontal tab\n', '\\      Backslash \\\n', '\\”      Double quotation mark “\n', '\\’      Single quotation mark ‘')
+
+a = int(input('Введите целое число : '))
+b = int(input('Введите второе целое число : '))
+print('Первое число', a, 'второе число', b)
+print('Сумма чисел: ', a + b)
+print('Деление чисел без остатка: ', a // b)
+print('Остаток от деления:  ', a % b)
+print('Вычесление степени a^b: ', a**b)
+
+
+
