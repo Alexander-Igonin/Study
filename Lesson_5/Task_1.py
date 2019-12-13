91d2b85ad30f887dd41349a4fdcc4ca63fb15512
Alexander-Igonin <55852571+Alexander-Igonin@users.noreply.github.com>
2019-12-13 13:11:56 +0200

    Add files via upload

---
+++ Lesson_5/Task_1.py
@@ -0,0 +1,14 @@
+rows = int(input('Высота фигуры: '))
+cols = rows * 2
+
+for i in range(rows):
+    for a in range(cols):
+        if i == 0 and a == cols // 2:
+            print('* ', end='')
+        elif i == rows - 1 and a > 0:
+            print('* ', end='')
+        elif a == cols // 2 - i or a == cols // 2 + i:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
